8c6baad71b2cf64aa2b0e44db08b60cd9f846c89
Aniyogakiza <aimable.niyogakiza@drpgroup.com>
2020-11-16 17:13:01 +0000

    Protocols in-progress

---
+++ recipe-api/producer-https-basics.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const server = require('fastify')({
+  https: {
+    key: fs.readFileSync(__dirname+`/tls/basic-private-key.key`),
+    cert: fs.readFileSync(__dirname+`/../shared/tls/basic-certificate.cert`),
+  }
+})
+
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || 4000
+
+console.log(`worker pid=${process.pid}`)
+
+server.get(`/recipes/:id`, async (req, reply) => {
+  console.log(`Worker request pid=${process.pid}`)
+  const id = Number(req.params.id)
+  if (id !== 42) {
+    reply.statusCode = 404
+    return { error: `not__found`}
+  }
+  return {
+    producer__pid: process.pid,
+    recipe: {
+      id,
+      name: 'Chicken Tikka Masala',
+      steps: 'Throw it in a pot',
+      ingredients: [
+        {id: 1, name: 'Chicken', quantity: '1 lb'},
+        {id: 2, name: 'Source', quantity: '2 cups'},
+      ]
+
+    }
+  }
+})
+
+server.listen(PORT, HOST, () => {
+  console.log(`Producer running at http://${HOST}:${PORT}`)
+})
--- web-api/consumer-http-basic.js
+++ web-api/consumer-http-basic.js
@@ -1,9 +1,16 @@
 const server = require('fastify')()
 const fetch = require('node-fetch')
+// const https = require('https')
+// const fs = require('fs')
 const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 3000
 const TARGET = process.env.TARGET || 'localhost:4000'
 
+// const options = {
+//   agent: new https.Agent({
+//     ca: fs.readFileSync(__dirname+`/../shared/tls/basic-certificate.cert`)
+//   })
+// }
 server.get('/', async () => {
   const req = await fetch(`http://${TARGET}/recipes/42`)
   const producer__data = await req.json()
